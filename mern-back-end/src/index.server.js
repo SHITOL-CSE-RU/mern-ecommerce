fdb99b71479e7157cd8d027d420a13aa6eee8ea5
SHITOL-CSE-RU <shitol.cse@gmail.com>
2022-04-26 00:01:58 +0600

    Model, Route, Controler creation

--- mern-back-end/src/index.server.js
+++ mern-back-end/src/index.server.js
@@ -1,18 +1,32 @@
 const express = require('express');
 const env = require('dotenv');
 const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+//routes
+const userRoutes = require('./routes/user');
 
 //environment variable or you can say constants
 env.config();
 
+//MongoDB Connections
+//mongodb+srv://root:<password>@cluster0.nd3v4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.nd3v4.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
+    {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true
+    }
+    ).then(() => {
+        console.log('Database Connected Success');
+    
+    });
 
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Hello from Server'
-    })
-})
+app.use(bodyParser());
+app.use('/api', userRoutes);
+
 
 app.listen(process.env.PORT, () => {
     console.log('listening on port ${process.env.PORT}');
-})
+});
---
+++ mern-back-end/src/routes/user.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.post('/signin', (req, res) => {
+
+
+
+});
+
+router.post('/signup', (req, res) => {
+
+
+
+});
+
+module.exports =router;
